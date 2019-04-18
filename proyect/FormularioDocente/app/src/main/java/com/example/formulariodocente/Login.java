250c00c6c06180f17909b7fd4da9741700917abc
luisSolares <luisitosolares7@gmail.com>
2019-04-18 16:04:21 -0400

    App

--- proyect/FormularioDocente/app/src/main/java/com/example/formulariodocente/Login.java
+++ proyect/FormularioDocente/app/src/main/java/com/example/formulariodocente/Login.java
@@ -4,17 +4,28 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+
 public class Login extends AppCompatActivity {
 
     private ProgressBar progreso;
     private FloatingActionButton fab;
     private View vista;
+    private TextInputEditText user, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +36,19 @@ public class Login extends AppCompatActivity {
         progreso = (ProgressBar) findViewById(R.id.progress_bar);
         fab = (FloatingActionButton) findViewById(R.id.fab);
 
-        ((View)findViewById(R.id.olvidoContra)).setOnClickListener(new View.OnClickListener() {
+        ((View) findViewById(R.id.olvidoContra)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               // Snackbar.make(vista, "Iniciando", Snackbar.LENGTH_SHORT).show();
+
+
+            }
+        });
+        ((View) findViewById(R.id.tengoCodigo)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 Intent intent = new Intent(Login.this, Verificacion_Codigo.class);
                 startActivity(intent);
-                }
+            }
         });
 
         fab.setOnClickListener(new View.OnClickListener() {
@@ -54,5 +71,42 @@ public class Login extends AppCompatActivity {
 
             ;
         }, 1000);
+    }
+
+    public void verificacionDatos() {
+        user = vista.findViewById(R.id.usuario);
+        password = vista.findViewById(R.id.contrasena);
+        Thread hilo = new Thread() {
+            @Override
+            public void run() {
+                String NAMESPACE = "";
+                String URL = "";
+                String METHOD_NAME = "";
+                String SOAP_ACTION = "";
+
+                SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
+                request.addProperty("user", user.getText() + "");
+                request.addProperty("password", password.getText() + "");
+
+                SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+                envelope.dotNet = true;
+
+                envelope.setOutputSoapObject(request);
+                HttpTransportSE transporte = new HttpTransportSE(URL);
+                try {
+                    transporte.call(SOAP_ACTION, envelope);
+                    SoapPrimitive resultado_xml = (SoapPrimitive) envelope.getResponse();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (XmlPullParserException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+
+        };
+        hilo.start();
     }
 }
