b40f45fffda7f18e14a813c229799709b856ce3f
Carlos Cardozo <carlitosv37@hotmail.com>
2019-04-19 18:32:10 -0400

    hola

--- proyect/FormularioDocente/app/src/main/java/com/example/formulariodocente/Cambiar_Contra.java
+++ proyect/FormularioDocente/app/src/main/java/com/example/formulariodocente/Cambiar_Contra.java
@@ -1,13 +1,22 @@
 package com.example.formulariodocente;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Cambiar_Contra extends AppCompatActivity {
-
+    private View vista;
+    private TextInputEditText Contraseña;
+    private TextInputEditText ContraseñaNueva;
+    private Button Confirmar1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cambiar__contra);
+        Contraseña=vista.findViewById(R.id.contraseña);
+        ContraseñaNueva=vista.findViewById(R.id.nuevacontraseña);
+        Confirmar1=(Button) findViewById(R.id.Confirmar);
     }
 }
