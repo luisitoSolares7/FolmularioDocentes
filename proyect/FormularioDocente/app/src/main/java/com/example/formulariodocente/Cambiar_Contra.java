cdd0645d54747ebf4a954a238182646fac4c6d44
Carlos Cardozo <carlitosv37@hotmail.com>
2019-04-19 17:32:35 -0400

    Modifiacion de la APP

---
+++ proyect/FormularioDocente/app/src/main/java/com/example/formulariodocente/Cambiar_Contra.java
@@ -0,0 +1,13 @@
+package com.example.formulariodocente;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cambiar_Contra extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cambiar__contra);
+    }
+}
--- proyect/FormularioDocente/app/src/main/java/com/example/formulariodocente/Login.java
+++ proyect/FormularioDocente/app/src/main/java/com/example/formulariodocente/Login.java
@@ -1,18 +1,15 @@
 package com.example.formulariodocente;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.formulariodocente.httpclient.HttpConnection;
@@ -22,14 +19,7 @@ import com.example.formulariodocente.modelos.Cuenta;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.ksoap2.SoapEnvelope;
-import org.ksoap2.serialization.SoapObject;
-import org.ksoap2.serialization.SoapPrimitive;
-import org.ksoap2.serialization.SoapSerializationEnvelope;
-import org.ksoap2.transport.HttpTransportSE;
-import org.xmlpull.v1.XmlPullParserException;
 
-import java.io.IOException;
 import java.util.Hashtable;
 
 public class Login extends AppCompatActivity {
