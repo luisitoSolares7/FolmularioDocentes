6d3dee75bb2977f78de21ceb1c567932859d35c0
luisitoSolares <luisitosolares7@gmail.com>
2019-06-03 19:23:42 -0400

    Creacion de Servicios para la App

---
+++ dev/SitioWeb/ServicioApp/Controllers/Formularios/FueraClasesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+[RoutePrefix("api/Formularios")]
+public class FueraClasesController : ApiController
+{
+    [HttpPost]
+    [Route("insertFueraClases")]
+    public HttpResponseMessage insertFormFueraClases([FromBody] FueraClases fueraClases)
+    {
+        HttpResponseMessage msg = null;
+        Boolean bandera = FueraClasesBRL.insertFormulario(fueraClases);
+        if (bandera)
+        {
+            msg = Request.CreateResponse<Boolean>(HttpStatusCode.OK,true);
+        }
+        else
+        {
+            msg = Request.CreateResponse<Cuenta>(HttpStatusCode.NotFound, null);
+        }
+        return msg;
+    }
+    [HttpPost]
+    [Route("getFueraClases")]
+    public HttpResponseMessage getFormFueraClase([FromBody] FueraClases fueraClases)
+    {
+        HttpResponseMessage msg = null;
+     
+        FueraClases form = FueraClasesBRL.getFormularioFueraClases(fueraClases.id);
+        if (form != null)
+        {
+                msg = Request.CreateResponse<FueraClases>(HttpStatusCode.OK, form);
+                return msg;
+         
+        }
+        else
+        {
+            msg = Request.CreateResponse<FueraClases>(HttpStatusCode.NotFound, form);
+        }
+
+        return msg;
+    }
+}
+
---
+++ dev/SitioWeb/ServicioApp/Controllers/Formularios/ListadoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ServicioApp.Controllers.Formularios
+{
+    [RoutePrefix("api/listado")]
+    public class ListadoController : ApiController
+    {
+        [HttpPost]
+        [Route("getFormularios")]
+        public HttpResponseMessage insertFormFueraClases([FromBody] Listado listado)
+        {
+            HttpResponseMessage msg = null;
+            List<Listado> listados = ListadoBRL.getListaPorCuenta(listado.id);
+            
+            if (listados!=null)
+            {
+                msg = Request.CreateResponse<List<Listado>>(HttpStatusCode.OK, listados);
+            }
+            else
+            {
+                msg = Request.CreateResponse<List<Listado>>(HttpStatusCode.OK, null);
+            }
+            return msg;
+        }
+    }
+}
