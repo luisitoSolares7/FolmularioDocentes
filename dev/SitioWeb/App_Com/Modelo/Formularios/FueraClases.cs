9541df39cb60af86bcbd671c842d227501dd0a99
luisitoSolares <luisitosolares7@gmail.com>
2019-06-03 19:27:26 -0400

    Creacion de modelos

---
+++ dev/SitioWeb/App_Com/Modelo/Formularios/FueraClases.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class FueraClases
+{
+    public int id { get; set; }
+    public DateTime fecha { get; set; }
+    public string materia { get; set; }
+    public string grupo { get; set; }
+    public string motivoActividad { get; set; }
+    public DateTime fechaActividad { get; set; }
+    public string descripActividad { get; set; }
+    public string lugarActividad { get; set; }
+
+    public FueraClases()
+    {
+    }
+
+    public FueraClases(int id, DateTime fecha, string materia, string grupo, string motivoActividad, DateTime fechaActividad, string descripActividad, string lugarActividad)
+    {
+        this.id = id;
+        this.fecha = fecha;
+        this.materia = materia;
+        this.grupo = grupo;
+        this.motivoActividad = motivoActividad;
+        this.fechaActividad = fechaActividad;
+        this.descripActividad = descripActividad;
+        this.lugarActividad = lugarActividad;
+    }
+    
+}
---
+++ dev/SitioWeb/App_Com/Modelo/Formularios/Listado.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Listado
+{
+    public int id { get; set; }
+    public int fkCuenta { get; set; }
+    public int fkTbl { get; set; }
+    public Boolean estado { get; set; }
+    public DateTime fecha { get; set; }
+    public int autorizador { get; set; }
+    public string nombre { get; set; }
+    public int tipo { get; set; }
+
+    public Listado()
+    {
+    }
+
+    public Listado(int id, int fkCuenta, int fkTbl, bool estado, string nombre, int tipo)
+    {
+        this.id = id;
+        this.fkCuenta = fkCuenta;
+        this.fkTbl = fkTbl;
+        this.estado = estado;
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+
+    public Listado(int id, int fkCuenta, int fkTbl, bool estado, DateTime fecha, int autorizador, string nombre, int tipo)
+    {
+        this.id = id;
+        this.fkCuenta = fkCuenta;
+        this.fkTbl = fkTbl;
+        this.estado = estado;
+        this.fecha = fecha;
+        this.autorizador = autorizador;
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+}
