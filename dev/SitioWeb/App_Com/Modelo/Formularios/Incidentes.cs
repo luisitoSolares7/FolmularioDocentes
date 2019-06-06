ac9de0c25378f75e94c2c8085406c0912cbef07a
luisitoSolares <luisitosolares7@gmail.com>
2019-06-06 16:47:38 -0400

    Creacion de Incidente modelo

---
+++ dev/SitioWeb/App_Com/Modelo/Formularios/Incidentes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Incidentes
+{
+    public int id { get; set; }
+    public DateTime fecha { get; set; }
+    public string descripcion { get; set; }
+
+    public Incidentes()
+    {
+    }
+
+    public Incidentes(int id, DateTime fecha, string descripcion)
+    {
+        this.id = id;
+        this.fecha = fecha;
+        this.descripcion = descripcion;
+    }
+}
