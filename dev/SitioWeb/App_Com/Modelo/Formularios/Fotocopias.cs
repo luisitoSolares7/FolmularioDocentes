2d4b100e3c9fdabbe067de8c9b24466804db06da
luisitoSolares <luisitosolares7@gmail.com>
2019-06-25 11:47:49 -0400

    Creacion y Actualizacion de modelos

---
+++ dev/SitioWeb/App_Com/Modelo/Formularios/Fotocopias.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Com.Modelo.Formularios
+{
+    public class Fotocopias
+    {
+        public int id { get; set; }
+        public DateTime fecha { get; set; }
+        public int cantidad { get; set; }
+        public string tipoDocuento { get; set; }
+        public string img1 { get; set; }
+        public string img2 { get; set; }
+        public string img3 { get; set; }
+        public string materia { get; set; }
+        public string Imagen { get; set; }
+
+
+
+    }
+}
--- dev/SitioWeb/App_Com/Modelo/Formularios/Listado.cs
+++ dev/SitioWeb/App_Com/Modelo/Formularios/Listado.cs
@@ -14,30 +14,37 @@ public class Listado
     public int autorizador { get; set; }
     public string nombre { get; set; }
     public int tipo { get; set; }
-
+    public int desicion;
     public Listado()
     {
     }
-
-    public Listado(int id, int fkCuenta, int fkTbl, bool estado, string nombre, int tipo)
+    public Listado(int id, int fkCuenta, int fkTbl, string nombre, int tipo,int desicion)
     {
         this.id = id;
         this.fkCuenta = fkCuenta;
         this.fkTbl = fkTbl;
-        this.estado = estado;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.desicion = desicion;
+    }
+    public Listado(int id, int fkCuenta, int fkTbl, string nombre, int tipo)
+    {
+        this.id = id;
+        this.fkCuenta = fkCuenta;
+        this.fkTbl = fkTbl;
         this.nombre = nombre;
         this.tipo = tipo;
     }
 
-    public Listado(int id, int fkCuenta, int fkTbl, bool estado, DateTime fecha, int autorizador, string nombre, int tipo)
+    public Listado(int id, int fkCuenta, int fkTbl, Boolean estado, DateTime fecha, string nombre, int tipo,int desecion)
     {
         this.id = id;
         this.fkCuenta = fkCuenta;
         this.fkTbl = fkTbl;
         this.estado = estado;
         this.fecha = fecha;
-        this.autorizador = autorizador;
         this.nombre = nombre;
         this.tipo = tipo;
+        this.desicion = desecion;
     }
 }
